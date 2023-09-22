c46de9c60b5eb6ba8f428d786ff901786e5fe625
IRESHALWIS <140914300+IRESHALWIS@users.noreply.github.com>
2023-09-22 21:11:03 +0530

    add  dependency
    install dependency

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-<<<<<<< Updated upstream
             input: ['resources/css/app.css', 'resources/js/app.js'],
-=======
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
->>>>>>> Stashed changes
             refresh: true,
         }),
     ],
